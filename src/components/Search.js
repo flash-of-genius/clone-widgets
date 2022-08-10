d42cad9c365d04ed0285ec8c5a6d56543cf9a8e4
flash-of-genius <amhdaar@gmail.com>
2022-08-10 06:52:07 +0100

    19 - Testing Execution: nly run once

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,11 @@ import React, { useState, useEffect } from "react";
 const Search = () => {
   const [term, setTerm] = useState("");
 
-  useEffect(() => {});
+  console.log("I run with every render!");
+
+  useEffect(() => {
+    console.log("I only run once!!");
+  }, []);
   return (
     <div>
       <div className="ui form">
